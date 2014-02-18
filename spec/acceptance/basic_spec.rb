e17b1f4ef31ce816226577717dac2d1102709b25
Phil Pham <phil.pham@puppetlabs.com>
2014-02-18 14:49:17 -0800

    remove legacy test

--- spec/acceptance/basic_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper_acceptance'
-
-describe 'basic tests' do
-  it 'copied the module' do
-    shell('ls /etc/puppet/modules/vcsrepo/Modulefile', {:acceptable_exit_codes => 0})
-  end
-end
